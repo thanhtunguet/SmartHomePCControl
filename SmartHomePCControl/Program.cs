4dee34b4f725413f1d3b7c8a0dd84af8f4fa9a17
thanhtunguet <thanhtung.uet@gmail.com>
2024-08-21 03:23:41 +0700

    Allow swagger

--- SmartHomePCControl/Program.cs
+++ SmartHomePCControl/Program.cs
@@ -15,12 +15,8 @@ builder.Services.AddScoped<WakeOnLanService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
@@ -30,5 +26,6 @@ app.UseResponseLogging();
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseCors();
 
 app.Run();
